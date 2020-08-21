cfbd858aefe7928606896e1ab8d81458d345a675
Margarita Morozova <ritamorozova@yahoo.com>
2020-08-21 17:15:37 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -19,6 +19,7 @@ puts "- play : lets you choose a song to play"
 puts "- exit : exits this program"
 end 
 
+
 def list(songs)
   songs.each_with_index do |song, index|
     puts "#{index+1}. #{song}"
@@ -38,9 +39,32 @@ def play(songs)
 end 
 
 
+def play(songs)
+  puts "Please enter a song name or number:"
+  user_input = gets.strip
+
+  if songs.include?(user_input)
+    puts "Playing #{user_input}"
+ 
+  else 
+    puts "Invalid input, please try again"
+  end 
+
+end 
+
+
+
+def list(songs)
+  songs.each_with_index do |song, index|
+    puts "#{index+1}. #{song}"
+  end 
+end 
+
+
 def exit_jukebox
   puts "Goodbye"
 end 
+
 
 def run(songs) 
  methods = ["help", "list", "play", "exit"]
@@ -63,6 +87,5 @@ def run(songs)
     exit_jukebox
     exit
   end
-
-  run(songs)
-end
+end 
+  
